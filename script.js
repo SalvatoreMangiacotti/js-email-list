29e2da5b0bbf00342051c4586abb5c53cacd8f50
Salvatore <workstudioms@gmail.com>
2025-01-22 13:28:30 +0100

    API tested in console, works as expected

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+// SELEZIONE OUTPUT PAGINA //
+
+const listaEmail = document.querySelector('.container ul');
+
+// GESTIONE API //
+
 const emailEndpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
 axios.get(emailEndpoint)
     .then(response => {
         // handle success
-        console.log(response);
+
+        const result = response.data;
+        console.log(result);
     })
     .catch(error => {
         // handle error
